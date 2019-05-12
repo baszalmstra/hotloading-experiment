0c4a57971da251a2dec96e9677366eba9c939d17
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-05-12 20:14:07 +0200

    use linefeed for a nice repl

--- crates/mun_cli/src/main.rs
+++ crates/mun_cli/src/main.rs
@@ -7,35 +7,39 @@ use std::io::Write;
 
 use mun_syntax::ast;
 use mun_syntax::ast::AstNode;
+use linefeed::{Interface, ReadResult};
 
 use diagnostic::Diagnostic;
 
-fn main() {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut input = String::new();
+fn main() -> Result<(), failure::Error> {
+    let term = Interface::new("mun")?;
 
-    loop {
-        print!("> ");
-        stdout.flush().unwrap();
-        input.clear();
-        stdin.read_line(&mut input).expect("Failed to read line");
-        let input = input.as_str().trim();
-        if input == ".quit" {
-            break;
+    term.set_prompt("> ")?;
+
+    println!("Welcome to the Mun CLI\nType :q and press enter to quit.");
+    while let ReadResult::Input(input) = term.read_line()? {
+        if !input.trim().is_empty() {
+            term.add_history_unique(input.clone());
         }
 
-        let line_index = line_index::LineIndex::new(&input);
-        let source = ast::SourceFile::parse(&input);
-        let errors = source.errors();
-        if errors.len() > 0 {
-            // TODO: Improve errors
-            for err in errors {
-                let diagnostic = Diagnostic::new(&line_index, err);
-                println!("{}", diagnostic);
+        match input.as_str() {
+            ":quit"|":q" => break,
+            _ => {
+                let line_index = line_index::LineIndex::new(&input);
+                let source = ast::SourceFile::parse(&input);
+                let errors = source.errors();
+                if errors.len() > 0 {
+                    // TODO: Improve errors
+                    for err in errors {
+                        let diagnostic = Diagnostic::new(&line_index, err);
+                        println!("{}", diagnostic);
+                    }
+                } else {
+                    print!("{}", source.syntax().debug_dump());
+                }
             }
-        } else {
-            print!("{}", source.syntax().debug_dump());
-        }
+        };
     }
+
+    Ok(())
 }
