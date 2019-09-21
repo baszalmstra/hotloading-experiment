8c7be83861424f0d5efed789bffc7de7b01f53f7
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-09-21 19:41:05 +0200

    added hacky stuff

--- crates/mun_codegen_ir/src/code_gen.rs
+++ crates/mun_codegen_ir/src/code_gen.rs
@@ -17,16 +17,19 @@ mod linker;
 pub fn write_module_shared_object(db: &impl IrDatabase, file_id: FileId) -> bool {
     let context = db.context();
     let module = db.module_ir(file_id);
+    let target = db.target();
 
     // Clone the module so we can modify it safely
     let llvm_module = module.llvm_module.clone();
+
+    // Generate startup procedure if not found
+    generate_startup_proc(&target, &llvm_module);
 
     // Generate the `get_symbols` method.
     gen_symbols(db, file_id, &module.functions, &llvm_module);
 
     Target::initialize_x86(&InitializationConfig::default());
 
-    let target = db.target();
     let path = Path::new("test.o");
     let llvm_target = Target::from_triple(&target.llvm_target).unwrap();
     let target_machine = llvm_target
@@ -162,4 +165,25 @@ fn gen_symbols(
     builder.position_at_end(&body_ir);
 
     builder.build_return(Some(&module_info));
+}
+
+fn generate_startup_proc(target: &spec::Target, module: &Module) {
+    if target.target_os == "windows" {
+        generate_dll_main_proc(module);
+    }
+}
+
+fn generate_dll_main_proc(module: &Module) {
+    let context = module.get_context();
+    let startup_type_fn = context.void_type().fn_type(&[], false);
+    let startup_fn = module.add_function(
+        "_DllMainCRTStartup",
+        startup_type_fn,
+        Some(Linkage::DLLExport),
+    );
+
+    let builder = context.create_builder();
+    let body_ir = startup_fn.append_basic_block("body");
+    builder.position_at_end(&body_ir);
+    builder.build_return(None);
 }
